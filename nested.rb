cfdb746e2dccfb64dd917988db6b992c1fcbd793
Bretton B Holmes <famunkwriter@me.com>
2019-10-19 14:08:46 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -58,7 +58,7 @@ def dennis_ritchies_language
      }
 		 programmer_hash = [:dennis_ritchie][:languages] = "C"
 
-		return dennis_ritchies_language
+		return programmer_hash
 end
 
 def adding_matz
