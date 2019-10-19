adffe67dfeabbbf57c81cad2879c737324678624
Bretton B Holmes <famunkwriter@me.com>
2019-10-19 14:51:36 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -127,7 +127,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-		 adding_to_dennis[:dennis_ritchie][:languages[1]] = "Assembly"
+		 programmer_hash[:dennis_ritchie][:languages[1]] = "Assembly"
 
 		 return programmer_hash
 
