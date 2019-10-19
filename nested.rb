c82e1870eb4fbec2983530f861d39d894ef42092
Bretton B Holmes <famunkwriter@me.com>
2019-10-19 14:32:08 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -83,8 +83,6 @@ def adding_matz
         }
      }
 		 programmer_hash = {:yukihiro_matsumoto => 1}
-		 programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
-		 programmer_hash[:yukihiro_matsumoto][:languages] = "LISP", "C"
 
 end
 
